4e6bdabeec191ea5bcde4dab395009341fa40e63
Jules <juleslordet@gmail.com>
2024-03-15 01:19:45 +0100

    ADD result section

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// animation texte performance
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const titleElement = document.querySelector('.title_result');
     const originalSize = 1;
@@ -16,22 +18,32 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     window.addEventListener('scroll', updateSize);
     updateSize();
-
-    let elementCompteur = document.getElementById('compteur');
-    let valeurMax = 137;
-    let duree = 3000; // Durée en millisecondes
-    let debut = Date.now();
-    let intervalle = duree / valeurMax;
-
-    let intervalID = setInterval(function() {
-        let tempsEcoule = Date.now() - debut;
-        let pourcentage = Math.floor(tempsEcoule / duree * valeurMax);
-
-        if (pourcentage <= valeurMax) {
-            elementCompteur.textContent = `${pourcentage}%`;
-        } else {
-            elementCompteur.textContent = `137%`;
-            clearInterval(intervalID);
-        }
-    }, intervalle);
 });
+
+// mis a jour de la date du site
+
+let date = new Date()
+let jour = String(date.getDate()).padStart(2, '0')
+let mois = String(date.getMonth() + 1).padStart(2, '0')
+let annee = date.getFullYear()
+let dateFormatee = "*Les informations citées ici ont été mises à jour à la date du " + jour + '/' + mois + '/' + annee + '.'
+
+const semaineContainer = document.getElementById(`res_semaine`)
+semaineContainer.textContent = dateFormatee
+
+
+// compteur de membre canal telegram
+
+const botToken = '6786917922:AAG5AKbnPz7sXxivUmW5LmAmD8JFY9Vm9j0';
+const chatId = '@auroranetwork';
+const url = `https://api.telegram.org/bot${botToken}/getChatMemberCount?chat_id=${chatId}`;
+
+const userContainer = document.getElementById('count_user')
+
+axios.get(url)
+.then(response => {
+    userContainer.textContent = response.data.result
+})
+.catch(error => {
+    console.error("Erreur lors de la récupération du nombre de membres :", error);
+})
